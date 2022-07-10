87993e3a5bb2b2b47f2cfbf40c30322ab718a46e
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-07-11 00:05:54 +0800

    change bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         14: '14px',
       },
       backgroundColor: {
-        'main-bg': '#FAFBFB',
+        'main-bg': '#cfd1d1',
         'main-dark-bg': '#20232A',
         'secondary-dark-bg': '#33373E',
         'light-gray': '#F7F7F7',
