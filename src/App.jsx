e2e853396a6ec14257f3345cfbf65a88cc59e105
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-07-12 00:10:59 +0800

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar opacity-30 dark:bg-secondary-dark-bg bg-white ">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
@@ -53,8 +53,8 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg bg-main-bg opacity-30 min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg opacity-80 dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
             <div className="fixed md:static bg-white border-b shadow-sm dark:bg-main-dark-bg navbar w-full ">
