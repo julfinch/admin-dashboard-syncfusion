a3feafafab6886908b13a87764c5de050c03bc5e
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-07-11 20:30:24 +0800

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -53,11 +53,11 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full  '
+                ? 'dark:bg-main-dark-bg bg-main-bg border-b min-h-screen md:ml-72 w-full  '
                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+            <div className="fixed md:static bg-nav-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
             <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       },
       backgroundColor: {
         'main-bg': '#e8e8e8',
+        'nav-bg': '#d4d4d4',
         'main-dark-bg': '#20232A',
         'secondary-dark-bg': '#33373E',
         'light-gray': '#F7F7F7',
