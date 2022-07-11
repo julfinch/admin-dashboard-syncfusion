91c0c8bd873886ccf8db54677859237d198421fb
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-07-12 02:39:02 +0800

    v1.1

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 
 import { Button } from '.';
-import { userProfileData } from '../data/dummy';
+import { cartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
-import avatar from '../data/avatar.jpg';
 
 const Cart = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div className="nav-item shadow absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
-      <div className="flex justify-between items-center">
-        <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
+      <div className="flex justify-between items-center p-4 ml-4">
+        <p className="font-semibold text-lg dark:text-gray-200">Shopping Cart</p>
         <Button
           icon={<MdOutlineCancel />}
           color="rgb(153, 171, 180)"
@@ -21,44 +21,44 @@ const Cart = () => {
           borderRadius="50%"
         />
       </div>
-      <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
-        <img
-          className="rounded-full h-24 w-24"
-          src={avatar}
-          alt="user-profile"
-        />
-        <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
-        </div>
-      </div>
+       
       <div>
-        {userProfileData.map((item, index) => (
+        {cartData.map((item, index) => (
           <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
             <button
               type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
               className=" text-xl rounded-lg p-3 hover:bg-light-gray"
             >
-              {item.icon}
+              {item.image}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+              <p className="font-semibold dark:text-gray-200 ">{item.name}</p>
+              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.category} </p>
+              <p className="font-semibold dart:text-gray-200 text-lg"> {item.price}</p>
             </div>
           </div>
         ))}
       </div>
+
+      <div className="flex justify-between items-center">
+        <p className="text-gray-500 ">Sub Total</p>
+        <p className="font-semibold ">$890</p>
+      </div>
+      <div className="flex justify-between items-center">
+        <p className="text-gray-500 ">Total</p>
+        <p className="font-semibold ">$890</p>
+      </div>
+
       <div className="mt-5">
         <Button
           color="white"
           bgColor={currentColor}
-          text="Logout"
+          text="Place Order"
           borderRadius="10px"
           width="full"
         />
+      </div>
       </div>
     </div>
 
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 
 import { Button } from '.';
-import { userProfileData } from '../data/dummy';
+import { chatData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
-import avatar from '../data/avatar.jpg';
 
 const Notification = () => {
   const { currentColor } = useStateContext();
@@ -12,7 +11,7 @@ const Notification = () => {
   return (
     <div className="nav-item shadow absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
-        <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
+        <p className="font-semibold text-lg dark:text-gray-200">Notifications</p>
         <Button
           icon={<MdOutlineCancel />}
           color="rgb(153, 171, 180)"
@@ -21,31 +20,20 @@ const Notification = () => {
           borderRadius="50%"
         />
       </div>
-      <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
-        <img
-          className="rounded-full h-24 w-24"
-          src={avatar}
-          alt="user-profile"
-        />
-        <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
-        </div>
-      </div>
+       
       <div>
-        {userProfileData.map((item, index) => (
+        {chatData.map((item, index) => (
           <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
             <button
               type="button"
               style={{ color: item.iconColor, backgroundColor: item.iconBg }}
               className=" text-xl rounded-lg p-3 hover:bg-light-gray"
             >
-              {item.icon}
+              {item.image}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
+              <p className="font-semibold dark:text-gray-200 ">{item.message}</p>
               <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
             </div>
           </div>
@@ -55,7 +43,7 @@ const Notification = () => {
         <Button
           color="white"
           bgColor={currentColor}
-          text="Logout"
+          text="See all notifications"
           borderRadius="10px"
           width="full"
         />
