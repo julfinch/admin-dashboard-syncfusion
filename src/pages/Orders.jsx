6fcd711b2cd89c3b509c41bc4ead5a3f914938e5
julfinch <julfinch@gmail.com>
2022-07-24 09:33:16 +0800

    final

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -7,8 +7,8 @@ import { Header } from '../components';
 const Orders = () => {
   const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
+    <div className="m-2 md:m-6 mt-24 p-2 md:p-6 rounded-3xl">
+      <Header category="Page" title="Orders"/>
       <GridComponent
         id="gridcomp"
         dataSource={ordersData}
